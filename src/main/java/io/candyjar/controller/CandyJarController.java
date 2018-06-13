fd1beeb7ada03900dcfdc856a2135d7e468e4a2d
John Killmer <killmer.john@gmail.com>
2018-06-13 17:50:36 -0400

    Adding unit tests for the rest of the get apis. Using JsonPath so I don't have to do string parsing

--- src/main/java/io/candyjar/controller/CandyJarController.java
+++ src/main/java/io/candyjar/controller/CandyJarController.java
@@ -10,37 +10,37 @@ import java.util.Optional;
 @RestController
 public class CandyJarController {
 
-	private CandyJarRepository repository;
+    private CandyJarRepository repository;
 
-	public CandyJarController(CandyJarRepository repository) {
-		this.repository = repository;
-	}
+    public CandyJarController(CandyJarRepository repository) {
+        this.repository = repository;
+    }
 
-	@GetMapping("/candy")
-	public List<Candy> getCandy() {
-		return repository.findAll();
-	}
+    @GetMapping("/candy")
+    public List<Candy> getCandy() {
+        return repository.findAll();
+    }
 
-	@GetMapping("/candy/{id}")
-	public Optional<Candy> getCandyById(@PathVariable Long id) {
-		return repository.findById(id);
-	}
+    @GetMapping("/candy/{id}")
+    public Optional<Candy> getCandyById(@PathVariable Long id) {
+        return repository.findById(id);
+    }
 
-	@GetMapping("/candy/name/{name}")
-	public Optional<Candy> getCandyByName(@PathVariable String name) {
-		return repository.findByName(name);
-	}
+    @GetMapping("/candy/name/{name}")
+    public Optional<Candy> getCandyByName(@PathVariable String name) {
+        return repository.findByName(name);
+    }
 
-	@PostMapping("/candy")
-	public Candy addCandy(@RequestBody Candy candy) {
-		return repository.save(candy);
-	}
+    @PostMapping("/candy")
+    public Candy addCandy(@RequestBody Candy candy) {
+        return repository.save(candy);
+    }
 
-	@PutMapping("/candy/{id}")
-	public Candy updateCandy(@PathVariable Long id, @RequestBody Candy candy) {
-		Candy repoCandy = repository.getOne(id);
-		repoCandy.setName(candy.getName());
-		repoCandy.setQuantity(candy.getQuantity());
-		return repository.save(repoCandy);
-	}
+    @PutMapping("/candy/{id}")
+    public Candy updateCandy(@PathVariable Long id, @RequestBody Candy candy) {
+        Candy repoCandy = repository.getOne(id);
+        repoCandy.setName(candy.getName());
+        repoCandy.setQuantity(candy.getQuantity());
+        return repository.save(repoCandy);
+    }
 }
--- src/test/java/io/candyjar/candyjar/io/candyjar/controller/CandyJarControllerTest.java
+++ src/test/java/io/candyjar/candyjar/io/candyjar/controller/CandyJarControllerTest.java
@@ -1,11 +1,16 @@
 package io.candyjar.candyjar.io.candyjar.controller;
 
+import com.jayway.jsonpath.JsonPath;
+import io.candyjar.model.Candy;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -17,8 +22,32 @@ public class CandyJarControllerTest {
     private TestRestTemplate restTemplate;
 
     @Test
-    public void testCandyApi() {
+    public void testGetAllCandy() {
         String body = this.restTemplate.getForObject("/candy", String.class);
-        assertThat(body).contains("Gummy Bear", "Swedish Fish", "Twix", "Sweettarts");
+
+        List<String> candy = JsonPath.read(body, "$..name");
+        assertThat(candy).containsOnly("Gummy Bear", "Swedish Fish", "Twix", "Sweettarts");
+    }
+
+    @Test
+    public void testGetCandyById() {
+        String body = this.restTemplate.getForObject("/candy/1", String.class);
+
+        Map actualCandy = JsonPath.read(body, "$");
+        assertThat(actualCandy)
+                .containsEntry("id", 1)
+                .containsEntry("name", "Gummy Bear")
+                .containsEntry("quantity", 25);
+    }
+
+    @Test
+    public void testGetCandyByName() {
+        String body = this.restTemplate.getForObject("/candy/name/Twix", String.class);
+
+        Map actualCandy = JsonPath.read(body, "$");
+        assertThat(actualCandy)
+                .containsEntry("id", 3)
+                .containsEntry("name", "Twix")
+                .containsEntry("quantity", 25);
     }
 }
