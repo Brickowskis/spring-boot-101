136291808d5e5dc6a5c48ffbebcca17dea0784cd
John Killmer <killmer.john@gmail.com>
2018-06-14 17:08:45 -0400

    Illustrating how to use DataJpaTest

---
+++ src/test/java/io/candyjar/candyjar/io/candyjar/controller/AddCandyTest.java
@@ -0,0 +1,45 @@
+package io.candyjar.candyjar.io.candyjar.controller;
+
+import io.candyjar.model.Candy;
+import io.candyjar.repository.CandyJarRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class AddCandyTest {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private CandyJarRepository repository;
+
+    @Test
+    public void testAddCandy(){
+
+        Optional<Candy> missingCandy = this.repository.findByName("Krackel");
+        assertThat(missingCandy.isPresent()).isFalse();
+
+        this.entityManager.persist(new Candy("Krackel", 45));
+
+        Candy actualCandy = this.repository.findByName("Krackel").get();
+        assertThat(actualCandy).isEqualTo(expectedCandy());
+
+    }
+
+    private Candy expectedCandy() {
+        Candy expectedCandy = new Candy("Krackel", 45);
+        expectedCandy.setId(5);
+        return expectedCandy;
+    }
+}
